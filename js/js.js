5d608898b2125153002132aff29ebe83d643b50e
Dijalma Silva <dijalmacz@gmail.com>
2017-01-07 10:39:02 -0300

    Adicionando transições em resumo profissional

--- js/js.js
+++ js/js.js
@@ -1,18 +1,34 @@
 $(document).ready(function (){
 
-  var positionDiv = $('#resume-about').offset();
-  console.log(positionDiv.left);
+  var positionResumeAbout = $('#resume-about').offset();
+  var positionExperience1 = $('#experience1').offset();
+  var positionExperience2 = $('#experience2').offset();
+  var positionEducation1 = $('#education1').offset();
 
     $(window).scroll(function (){
-      if ($(this).scrollTop() > positionDiv.left){
-        addAnime($('#resume-about'));
+      var positionScroll = $(this).scrollTop();
+      var diference = $(window).height() + 40;
+      if (positionScroll > positionResumeAbout.top - diference){
+        addAnime($('#resume-about'), 'bounceInUp');
+      }
+
+      if (positionScroll > positionExperience1.top - diference){
+        addAnime($('#experience1'), 'bounceInRight');
+      }
+
+      if (positionScroll > positionExperience2.top - diference){
+        addAnime($('#experience2'), 'bounceInLeft');
+      }
+
+      if (positionScroll > positionEducation1.top - diference){
+        addAnime($('#education1'), 'bounceInRight');
       }
     });
 
 });
 
 
-function addAnime(input){
+function addAnime(input, effect){
 
-  $(input).addClass('animated bounceInUp');
+  $(input).addClass('animated '+ effect);
 }
